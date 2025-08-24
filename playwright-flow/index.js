0ff20bd9ef4f39cbf113859def5fa6f4c36ccfa1
Pradeep Patil <meetpradeepp@gmail.com>
2025-08-24 22:45:06 +0530

    Update index.js

--- playwright-flow/index.js
+++ playwright-flow/index.js
@@ -37,37 +37,3 @@ console.log('Axe-core keys:', Object.keys(axe));
   console.log(JSON.stringify(steps));
 })();
 
-/*
-const playwright = require('playwright');
-
-const { injectAxe, getViolations } = require('@axe-core/playwright');
-
-module.exports = async function () {
-  const url = 'https://www.google.com'; // Change to the URL you want to test
-  const steps = [];
-  
-  // Launch browser
-  const browser = await playwright.chromium.launch();
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle' });
-
-  // Inject axe-core using playwright-axe & run it
-  await injectAxe(page);
-  const axeResult = await page.evaluate(async () => await window.axe.run());
-
-  // Run pa11y
-  const pa11yResult = await pa11y(url);
-
-  await browser.close();
-
-  // Return results in the expected format
-  steps.push({
-    step: 'landing_page',
-    axeResult: axeResult,
-    pa11yResult: pa11yResult,
-  });
-  
-  // Return results for each step
-  return steps;
-};
-*/
