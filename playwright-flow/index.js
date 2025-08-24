7e74616079ee7fe6d48463d7263d5b96ab8fd7bb
Pradeep Patil <meetpradeepp@gmail.com>
2025-08-24 21:37:30 +0530

    Update index.js
    Complete rewamp to use new approach of axe

--- playwright-flow/index.js
+++ playwright-flow/index.js
@@ -1,7 +1,45 @@
 // playwright-flow/index.js
 
-const playwright = require('playwright');
+// index.js (or main.js)
+// Requires: playwright, @axe-core/playwright, pa11ty
+
+const { chromium } = require('playwright');
 const pa11y = require('pa11y');
+const { injectAxe, getAxeResults } = require('@axe-core/playwright');
+
+
+(async () => {
+  const url = 'https://www.google.com'; // Change to the URL you want to test
+  const steps = [];
+  
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  // Run axe
+  await injectAxe(page);
+  const axeResult = await getAxeResults(page);
+  
+  // Run pa11y
+  const pa11yResult = await pa11y(url);
+
+  // Housekeeping close browser
+  await browser.close();
+
+  // Return results in the expected format
+  steps.push({
+    step: 'landing_page',
+    axeResult: axeResult,
+    pa11yResult: pa11yResult,
+  });
+  
+  // Print results as JSON to stdout
+  console.log(JSON.stringify(steps));
+})();
+
+/*
+const playwright = require('playwright');
+
 const { injectAxe, getViolations } = require('@axe-core/playwright');
 
 module.exports = async function () {
@@ -32,3 +70,4 @@ module.exports = async function () {
   // Return results for each step
   return steps;
 };
+*/
